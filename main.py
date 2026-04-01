5480b82928983bc663963ab1a4c310442cdb51fa
CodeWithJazmine <71017555+CodeWithJazmine@users.noreply.github.com>
2026-04-01 14:26:02 -0700

    Add a panda to represent the player and refactor camera for 3rd person view

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from math import pi, sin, cos
 
 from direct.showbase.ShowBase import ShowBase
@@ -19,13 +20,11 @@ class MyApp(ShowBase):
         self.scene.setScale(0.25,0.25,0.25)
         self.scene.setPos(-8,42,0)
 
-        # Add the spinCameraTask procedure to the task manager
-        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
-
+    
         # Load and transform the panda actor
         self.pandaActor = Actor("models/panda-model",
                                 {"walk": "models/panda-walk4"})
-        self.pandaActor.setScale(0.005,0.005,0.005)
+        self.pandaActor.setScale(0.005, 0.005, 0.005)
         self.pandaActor.reparentTo(self.render)
         # Loop its animation
         self.pandaActor.loop("walk")
@@ -36,20 +35,28 @@ class MyApp(ShowBase):
         hprInterval1 = self.pandaActor.hprInterval(3, Point3(180,0,0), startHpr=Point3(0,0,0))
         hprInterval2 = self.pandaActor.hprInterval(3, Point3(0,0,0), startHpr=Point3(180,0,0))
 
-        #Create and play the sequence that coordinates the intervals
+        # Create and play the sequence that coordinates the intervals
         self.pandaPace = Sequence(posInterval1, hprInterval1, 
                                   posInterval2, hprInterval2,
                                   name="pandaPace")
-
         self.pandaPace.loop()
 
-    # Define a procedure to move the camera
-    def spinCameraTask(self, task):
-        angleDegrees = task.time * 6.0
-        angleRadians = angleDegrees * (pi/180.0)
-        self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
-        self.camera.setHpr(angleDegrees,0,0)
-        return Task.cont
+        # Load another panda to control with the arrow keys
+        self.characterPanda = Actor("models/panda-model")
+        self.characterPanda.setScale(0.005, 0.005, 0.005)
+        self.characterPanda.reparentTo(self.render)
+
+        # Set camera position relative to character
+        # Disable mouse control of camera
+        self.disableMouse()
+        self.camera.reparentTo(self.characterPanda)
+        self.camera.setPos(0, 1500, 1500) 
+        self.camera.setHpr(180,-15,0)
+        self.camLens.setFov(80)
+        
+        # Keyboard inputs
+        self.accept('escape', sys.exit)
+
 
         
 app = MyApp()
